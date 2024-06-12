9388ea0894f2d5657efe6e5de6bc7d0a105a5c04
chinmaypaliwal1 <chinmaypaliwal96@gmail.com>
2024-06-12 18:26:23 +0530

    first commit

---
+++ src/components/weather.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+
+
+function weatherApp(){
+    const todatefunction = () =>{
+        const month =[
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
+        const weakdays =[
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+        ];
+
+    }
+    return(
+        <div>
+
+        </div>
+    )
+}
